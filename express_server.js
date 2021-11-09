dcba620ae0dfbe3bd14410fc95ea7ce63279889a
Kyle Fagnan <kfagnan19@gmail.com>
2021-11-08 20:25:46 -0700

    add redirect to new page

--- express_server.js
+++ express_server.js
@@ -46,8 +46,11 @@ app.get("/hello", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortRandm = generateRandomString();
+  urlDatabase[shortRandm] = req.body.longURL;
+  res.redirect(`/urls/${shortRandm}`);         // redirect to new page
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
